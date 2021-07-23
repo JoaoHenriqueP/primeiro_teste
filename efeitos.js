6fe0c68566fac92ae66358c43c2374d7b1fea93e
João <joao.pinto@compila.com.br>
2021-07-23 13:49:58 -0300

    adicionando efeitos.js

---
+++ efeitos.js
@@ -0,0 +1,7 @@
+if (boletoNumeros.length == 47) {
+        var somaTot = 0
+        for (let cont = 0; cont <= valor.length - 1; cont++) {
+            let multiplicadores = [8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+            let multiplicacao = multiplicadores[cont] * valor[cont]
+            somaTot = somaTot + multiplicacao
+        }
